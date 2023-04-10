734dbda6995c28f750dbc61a1939859a84e3057b
Jessy Acuña Rodríguez <123852325+AHobbitTalksData@users.noreply.github.com>
2023-04-09 18:10:39 -0600

    Update bigcountries.sql

--- Basic projects/BigCountries/bigcountries.sql
+++ Basic projects/BigCountries/bigcountries.sql
@@ -12,7 +12,7 @@ gdp bigint,
 PRIMARY KEY (name)
 );
 
-/* Adding information into the table we jsut created */ 
+/* Adding information into the table*/ 
 insert into world (name, continent, area, population, gdp)
 values 
 	( 'Afghanistan', 'Asia', 652230, 25500100, 20343000000),
@@ -20,5 +20,6 @@ values
     ('Algeria', 'Africa', 2381741, 37100000, 188681000000),
     ('Andorra', 'Europe', 468, 78115, 3712000000),
     ('Angola', 'Africa', 1246700, 20609294, 100990000000);
-	
+
+/* Filtering according to instructions*/
 SELECT name, population, area from world WHERE area>=3000000 or population >= 25000000;
