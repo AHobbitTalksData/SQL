75e2a718868d28c9bf1a3ef1738f1c01c0a39267
Jessy Acuña Rodríguez <123852325+AHobbitTalksData@users.noreply.github.com>
2023-04-21 23:45:36 -0600

    Update quiz3.sql

--- SQL for Data Science/quiz3.sql
+++ SQL for Data Science/quiz3.sql
@@ -19,3 +19,35 @@ FROM tracks
 INNER JOIN albums ON tracks.AlbumID = albums.AlbumID
 INNER JOIN artists ON albums.ArtistID = artists.ArtistID
 WHERE artists.Name = 'Audioslave';
+
+----------------------------------------------------------------------------------------------------------------
+
+-- Q3) Find the first and last name of any customer who does not have an invoice. 
+-- Are there any customers returned from the query? R/ Yes 
+
+SELECT customers.FirstName, customers.LastName 
+FROM Customers
+INNER JOIN invoices ON customers.CustomerID = Invoices.CustomerID
+WHERE invoices.InvoiceID IS NULL;
+
+----------------------------------------------------------------------------------------------------------------
+
+-- Q4) Find the total price for each album. What is the total price for the album “Big Ones”? R/ 14.85
+
+SELECT SUM(tracks.UnitPrice), albums.AlbumID, albums.Title
+FROM tracks
+INNER JOIN albums ON tracks.AlbumID = albums.AlbumID
+INNER JOIN artists ON albums.ArtistID = artists.ArtistID
+WHERE albums.Title = 'Big Ones'
+GROUP BY albums.AlbumID, albums.Title;
+
+----------------------------------------------------------------------------------------------------------------
+
+-- Q5) How many records are created when you apply a Cartesian join to the invoice and invoice items table?
+-- Only 25 records will be shown in the output so please look at the bottom of the output to see how many records were retrieved. R/ 922880 
+
+SELECT COUNT(*)
+FROM invoices
+CROSS JOIN invoice_items
+
+
